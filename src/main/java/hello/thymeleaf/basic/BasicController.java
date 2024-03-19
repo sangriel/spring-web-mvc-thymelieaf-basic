2cefe8f49793b6936315b1608b55965893e800d0
sangmin han <sangriel@naver.com>
2024-03-19 22:45:13 +0900

    반복

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -107,6 +107,21 @@ public class BasicController {
         return "basic/attribute";
     }
 
+    @GetMapping("/each")
+    public String each(Model model) {
+        addUsers(model);
+        return "basic/each";
+    }
+
+    private void addUsers(Model model) {
+        List<User> list = new ArrayList<>();
+        list.add(new User("userA", 10));
+        list.add(new User("userB", 10));
+        list.add(new User("userC", 10));
+        model.addAttribute("users", list);
+    }
+
+
     @Data
     static class User {
         private String username;
